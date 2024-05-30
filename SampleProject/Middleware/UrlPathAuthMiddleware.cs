e2ece3a48e720ffa6fcf109e6f885f525693fb60
David <test@mail.com>
2024-05-30 17:26:55 +0800

    1.url path權限完成 2.Hangfire初版調適

---
+++ SampleProject/Middleware/UrlPathAuthMiddleware.cs
@@ -0,0 +1,71 @@
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Primitives;
+
+namespace SampleProject.Middleware;
+
+/// <summary>
+/// Url Path 認證機制
+/// </summary>
+public class UrlPathAuthMiddleware(RequestDelegate next, IConfiguration configuration)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var authInfo = configuration.GetSection("SystemOption:MiddlewareAuthConfig:AuthInfo");
+
+        var headers = context.Request.Headers;
+
+        var authStatus = false;
+        if (headers.TryGetValue("Authorization", out var authStr))
+        {
+            var authHeader = authStr.ToString();
+
+            var baseAuthInfo = AuthenticationHeaderValue.Parse(authHeader);
+
+            var authScheme = baseAuthInfo.Scheme;
+            var authParameter = baseAuthInfo.Parameter ?? "";
+
+            var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authParameter)).Split(':');
+
+            //取出設定檔內容
+            var validAccount = authInfo.GetValue<string>("Account", "");
+            var passwordInfo = authInfo.GetSection("PasswordInfo");
+
+            //是否為動態密碼，為動態密碼時，密碼為當下時間（ex:目前為下午3點33分，密碼為1533）
+            var isDynamic = passwordInfo.GetValue("IsDynamic", true);
+            var validPassword =
+                isDynamic ? DateTime.Now.ToString("HHm") : passwordInfo.GetValue<string>("Password", "");
+
+            var username = credentials.First();
+            var password = credentials.Last();
+
+            //驗證帳密
+            if (authScheme.Equals("Basic") &&
+                username == validAccount &&
+                password == validPassword)
+            {
+
+                //建立Client User
+                var claims = new[] {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role,"gust")
+                };
+                var identity = new ClaimsIdentity(claims, authScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                context.User = principal;
+                authStatus = true;
+            }
+        }
+
+        if (authStatus)
+        {
+            await next(context);
+            return;
+        }
+
+        context.Response.StatusCode = 401;
+        context.Response.Headers.WWWAuthenticate = "Basic";
+    }
+}
--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -5,7 +5,11 @@ using Autofac.Extensions.DependencyInjection;
 using Dommel;
 using ElmahCore;
 using ElmahCore.Mvc;
+using Hangfire;
+using Hangfire.MySql;
+using Hangfire.Storage.SQLite;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
 using Oracle.ManagedDataAccess.Client;
 using SampleProject.Base.Interface.DB.Repositories;
 using SampleProject.Base.Util;
@@ -15,6 +19,7 @@ using SampleProject.Helpers;
 using SampleProject.Interface.Elmah;
 using SampleProject.Middleware;
 using SampleProject.Services.DB.User;
+using SQLite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,10 +30,11 @@ builder.Services.AddScoped<IBaseDbConnection, BaseDbConnection>();
 
 //使用 AutoFuc注入符合命名空間的class
 var config = builder.Configuration;
+var env = builder.Environment;
+
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>(containerBuilder =>
     {
-   
         var fileTypeRegex = new Regex(@".(Services|Repositories)");
         var assembly = Assembly.GetExecutingAssembly();
         containerBuilder.RegisterAssemblyTypes(assembly)
@@ -37,8 +43,81 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
                                fileTypeRegex.IsMatch(fileType.Namespace))
             .AsSelf()
             .InstancePerLifetimeScope();
-        
     });
+
+// 取得專案目錄路徑
+var projectDirectory = Directory.GetCurrentDirectory();
+
+//系統設定
+var systemOptionDictionary = builder.Configuration.GetSection("SystemOption").GetChildren().ToDictionary(x => x.Key);
+
+#region Hangfire設定
+
+var hangfireConfig = systemOptionDictionary.GetValueOrDefault("HangfireConfig");
+var hangfireStatus = false;
+if (hangfireConfig != null)
+{
+    hangfireStatus = hangfireConfig.GetValue("IsOn", false);
+
+    if (hangfireStatus)
+    {
+        builder.Services.AddHangfire(configuration =>
+        {
+            configuration.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings();
+
+            var hangfireStorageType = hangfireConfig.GetValue<string>("Driver", "");
+            var hangfireStorageDictionary = hangfireConfig.GetSection("StorageInfo");
+            var hangfireStorageInfo = hangfireStorageDictionary.GetSection(hangfireStorageType);
+
+            var dbConnectStr = "";
+            switch (hangfireStorageType)
+            {
+                default:
+                    hangfireStatus = false;
+                    break;
+
+                case "Sqlite":
+
+                    var dbName = hangfireStorageInfo.GetValue<string>("DbName") ?? "hangfire.db";
+                    var dbPath = hangfireStorageInfo.GetValue<string>("DbPath");
+
+                    if (!string.IsNullOrEmpty(dbPath))
+                    {
+                        // 連結 hangfire 資料夾路徑
+                        var hangfireDirectory = Path.Combine(projectDirectory, dbPath);
+
+                        // 檢查hangfire資料夾是否存在，如果不存在則建立
+                        if (!Directory.Exists(hangfireDirectory))
+                        {
+                            Directory.CreateDirectory(hangfireDirectory);
+                        }
+
+                        dbConnectStr = Path.Combine(env.ContentRootPath, "Hangfire", "hangfire.db");
+                    }
+                    else
+                    {
+                        dbConnectStr = dbName;
+                    }
+
+                    configuration.UseSQLiteStorage(dbConnectStr);
+                    break;
+
+                case "Mysql":
+
+                    configuration.UseStorage(new MySqlStorage(
+                        hangfireStorageInfo.GetValue<string>("Connection",""),
+                        new MySqlStorageOptions { TablesPrefix = "Hangfire" }));
+                    break;
+            }
+        });
+    }
+}
+
+#endregion
+
+builder.Services.AddHangfireServer();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -61,11 +140,14 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-//Elmah設定
-var elmahStatus = config.GetValue<bool>("SystemOption:Elmah:status");
-var elmahUrlStatus = config.GetValue<bool>("SystemOption:Elmah:urlStatus");
+#region Elmah
 
-if (elmahStatus)
+//Elmah設定
+var elmahConfig = systemOptionDictionary.GetValueOrDefault("ElmahConfig");
+var elmahIsOn = elmahConfig.GetValue<bool>("IsOn", false);
+var elmahUrlStatus = elmahConfig.GetValue<bool>("UrlPathOn", false);
+
+if (elmahIsOn)
 {
     builder.Services.AddElmah<XmlFileErrorLog>(options =>
     {
@@ -75,16 +157,21 @@ if (elmahStatus)
         {
             options.Path = "logs";
         }
+
         options.Notifiers.Add(new NotificationFilter());
         options.Filters.Add(new CmsErrorLogFilter());
     });
 }
+
+#endregion
 
 builder.Services.AddControllers(options =>
 {
     //加入自訂的model檢查
     options.Filters.Add<ModelValidationAttribute>();
 });
+
+#region 專案資源資料夾
 
 //資料夾初始化
 var pathDatas = builder.Configuration.GetSection("FilePath").GetChildren();
@@ -117,7 +204,32 @@ if (pathDatas.Any())
     }
 }
 
+#endregion
+
 var app = builder.Build();
+
+#region UrlPath驗證設定
+
+var middlewareAuthConfig = systemOptionDictionary.GetValueOrDefault("MiddlewareAuthConfig");
+if (middlewareAuthConfig != null)
+{
+    var middlewareAuthStatus = middlewareAuthConfig.GetValue<bool>("IsOn", false);
+
+    //取的需要登入的路由清單
+    var urlPathList = middlewareAuthConfig.GetSection("UrlPaths").Get<IEnumerable<string>>() ?? [];
+
+    //啟用驗證檢查
+    if (middlewareAuthStatus)
+    {
+        app.UseWhen(context =>
+                urlPathList.Any(x =>
+                    context.Request.Path.ToString().Split("/", StringSplitOptions.RemoveEmptyEntries).First()
+                        .Contains(x, StringComparison.CurrentCultureIgnoreCase)),
+            appBuilder => { appBuilder.UseMiddleware<UrlPathAuthMiddleware>(); });
+    }
+}
+
+#endregion
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
@@ -132,13 +244,24 @@ if (app.Environment.IsDevelopment())
     app.UseCors("AllowSpecificOrigin");
 }
 
-if (elmahStatus)
+if (elmahIsOn)
 {
-    app.UseElmah();  
+    app.UseElmah();
 }
 
 app.UseStaticFiles();
 
+#region Hangfire
+
+if (hangfireStatus)
+{
+    //Hangfire相關啟用設定
+    app.UseHangfireDashboard("/hangfire");
+}
+
+#endregion
+
+app.UseMiddleware<UrlPathAuthMiddleware>();
 app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseMiddleware<ExceptionHandleMiddleware>();
 // app.UseAuthentication();
